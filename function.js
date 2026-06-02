3c8c0d304c29115497b64db459862983cb076514
harshal1800 <harshalchavhank18@gmail.com>
2026-06-02 21:33:32 +0530

    write some functions

--- function.js
+++ function.js
@@ -5,3 +5,15 @@
         console.log(c);
     }
     sum();
+
+
+    console.log("we are learning git and github");
+
+    "hey".toUpperCase();
+    console.log("hey".toUpperCase());
+
+    [1,2,3].push(4);
+    console.log([1,2,3].push(4));
+
+
+
